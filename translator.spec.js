4ebe4a310c62b7ff4e6189b63dd4bd41c600c96f
DorBia <dorota.bialy95@gmail.com>
2022-09-01 23:05:01 +0100

    Added basic tests

--- translator.spec.js
+++ translator.spec.js
@@ -1,32 +1,40 @@
 import {translateToMorse} from "./translator";
 
 describe("Testing translateToMorse", () => {
-    it("a should translate to be *-", () => {
-        //Arange
-        let string;
-        //Act
-        string = "a";
-        let result = translateToMorse(string);
+    it("should translate single letters, eg. 'a' should be '.-'", () => {
+        //Arrange and act
+        const result = translateToMorse("a");
         //Assert
-        expect(result).toBe("*-");
+        expect(result).toBe(".-");
     });
 
-    it("should translate rob to be .-. --- -...", () => {
-        //Arange
-        let string;
-        //Act
-        string = "rob";
-        let result = translateToMorse(string);
-        //Assert
-        expect(result).toBe(".-. --- -...");
+    it("should translate simple words, eg. 'welcome' should be '.-- . .-.. -.-. --- -- .'", () => {
+        const result = translateToMorse("welcome");
+        expect(result).toBe(".-- . .-.. -.-. --- -- .");
     });
+
+    it("should translate numbers, eg. '123' should be '.---- ..--- ...--'", () => {
+        const result = translateToMorse("123");
+        expect(result).toBe(".---- ..--- ...--");
+    });
+
+    it("should accept capital letters, eg. 'HELLO' should be '.... . .-.. .-.. ---'", () => {
+        const result = translateToMorse("HELLO");
+        expect(result).toBe(".... . .-.. .-.. ---");
+    });
+
+    it("should translate simple sentence", () => {
+        const result = translateToMorse("Hello from the VS Code side");
+        expect(result).toBe(".... . .-.. .-.. --- / ..-. .-. --- -- / - .... . / ...- ... / -.-. --- -.. . / ... .. -.. .");
+    });
+
 });
 
 // What should translateToMorse function actually be able to do?
 
-// - Receive letters and translate those letters into morse code
-// - Take into consideration capital letters - these also need to translate to morse code
-// - Take into consideration spaces between words and translate them
-// - Numbers!!!
-// - Symbols??
+// - Receive letters and translate those letters into morse code ✔️
+// - Take into consideration capital letters - these also need to translate to morse code ✔️
+// - Take into consideration spaces between words and translate them ✔️
+// - Numbers!!! ✔️
+// - Symbols?? 
 // - Extension - different language
