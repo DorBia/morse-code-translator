96d501a197b42b8c7106558ccab362f5750a4d00
DorBia <dorota.bialy95@gmail.com>
2022-09-02 16:11:32 +0100

    Translating both ways on keyup

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-import {translateToMorse} from "./translator.js";
+import {translateToMorse, translateToText} from "./translator.js";
 
 const translate = document.querySelector(".translate input");
 const translationContainer = document.querySelector(".container");
 
 const addTranslation = () => {
-    const translated = translateToMorse(translate.value);
+    let translated = ""
+
+    const pattern = /^[-. /]{1,}$/;
+    if(pattern.test(translate.value)) {
+        translated = translateToText(translate.value);
+    } else {
+        translated = translateToMorse(translate.value);
+    }
     translationContainer.innerHTML = `<p>${translated}</p>`
 }
 
-translate.addEventListener("keyup", () => {
-    addTranslation();
-});
+translate.addEventListener("keyup", addTranslation);
