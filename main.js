7878e1540aed8976f377c65e29f994c7b0f90bdb
DorBia <dorota.bialy95@gmail.com>
2022-09-04 15:57:46 +0100

    Code cleanup and adding favicon

--- main.js
+++ main.js
@@ -1,72 +1,74 @@
 import {translateToMorse, translateToText} from "./translator.js";
 
-const translate = document.querySelector(".translate");
-const outputBox = document.querySelector(".output");
-const copy = document.querySelector(".copy");
-const playButton = document.querySelector(".play-sound");
+/* ----- DOM ----- */
+const toTranslate = document.querySelector(".input__text");
+const outputBox = document.querySelector(".output__text");
+const copy = document.querySelector(".output__copy");
+const playButton = document.querySelector(".output__play-sound");
 
+
+/* ----- FUNCTIONS ----- */
+
+// "print" translation to the output box
 const addTranslation = () => {
-    let translated = ""
+    let translated = "";
     const pattern = /^[-. /]{1,}$/;
     
-    if(pattern.test(translate.value)) {
-        translated = translateToText(translate.value);
+    if(pattern.test(toTranslate.value)) {
+        translated = translateToText(toTranslate.value);
         playButton.style.display = "none";
 
     } else {
-        translated = translateToMorse(translate.value);
+        translated = translateToMorse(toTranslate.value);
         playButton.style.display = "block";
     }
     outputBox.innerHTML = translated;
 }
 
-translate.addEventListener("keyup", e => {
-    e.preventDefault();
-    if (e.key === "Enter") void(0);
-    addTranslation()
-});
-
-copy.addEventListener("click", () => {
-    outputBox.select();
-    outputBox.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(outputBox.textContent);
-});
-
-// using src rather than new sounds for Safari and iOS, otherwise it doesn't work
+// play sound - using src rather than new sounds for Safari and iOS, otherwise it doesn't work
 const playMorseCode = () => {
-    const sound = new Audio()
-    
+    const sound = new Audio();
     const string = outputBox.textContent;
+
     let index = 0;
     const playAudio = (audio) => {
-        if (!audio || !(audio instanceof Audio)) return;
         
         audio.onended = () => {
             if(string[index] === ".") {
-                sound.src = "./media/morse-short.wav"
+                sound.src = "./media/morse-short.wav";
                 playAudio(sound);
             } else if(string[index] === "-") {
-                sound.src = "./media/morse-long.wav"
+                sound.src = "./media/morse-long.wav";
                 playAudio(sound);
             } else if(string[index] === " ") {
-                sound.src = "./media/silent-short.wav"
+                sound.src = "./media/silent-short.wav";
                 playAudio(sound);
             } else if(string[index] === "/") {
-                sound.src = "./media/silent-long.wav"
+                sound.src = "./media/silent-long.wav";
                 playAudio(sound);
             }
             index++;
         }
         audio.play();
-        
     }
-    sound.src = "./media/silent-short.wav"
-    playAudio(sound)
+    sound.src = "./media/silent-short.wav";
+    playAudio(sound);
 }
+
+/* ----- EVENTS AND BUTTONS ----- */
+
+toTranslate.addEventListener("keyup", addTranslation);
+
+copy.addEventListener("click", () => {
+    outputBox.select();
+    outputBox.setSelectionRange(0, 99999); // for mobile
+    navigator.clipboard.writeText(outputBox.textContent);
+});
 
 playButton.addEventListener("click", () => {
     playMorseCode();
+    playButton.style.display = "none";
+    // disable button anyway, prevents from adding a letter and playing again immediately
     playButton.disabled = true;
     setTimeout(() => playButton.disabled = false, 5000);
-    playButton.style.display = "none";
 });
